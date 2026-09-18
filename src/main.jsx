f14aa0aef2187d796f4f18c0dc17d827d59e1d3d
Thunderbeast5 <163726116+Thunderbeast5@users.noreply.github.com>
2026-09-18 22:43:50 +0530

    developer details

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import './lib/firebase.js'
 
+console.log(
+  "%cDeveloped by Vedant Purkar | vedant.purkar05@gmail.com",
+  "font-weight: bold;"
+);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
